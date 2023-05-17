9c4c1d0329a943429b87523f012b5492bd918bc5
Omid Ali Heidari <omidaliheidari@gmail.com>
2023-05-17 06:46:47 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// doğum tarihi girilen kişinin yaşını hesaplama
+Console.Write("Lütfen doğum tarihini (gün.ay.yıl) gir: ");
+DateTime birthDay = Convert.ToDateTime(Console.ReadLine());
+
+TimeSpan ageCalculator = (DateTime.Now - birthDay);
+Console.WriteLine(ageCalculator.Days / 365);
